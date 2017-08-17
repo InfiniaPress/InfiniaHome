2fa21f5446aa1d03fb379ad5f0f583fd6508845b
xiurobert <xiurobert@gmail.com>
2017-08-17 21:26:34 +0800

    some minor stuff

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '192.168.0.112'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'infiniahome'),
+            'username' => env('DB_USERNAME', 'infinia'),
+            'password' => env('DB_PASSWORD', 'infinia'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
